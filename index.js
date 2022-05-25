525251e29c3d7e61411daf5426a22474ea9334ef
Mikalai Zarazaka <golmtz@gmail.com>
2022-05-25 13:04:58 +0300

    add port & url

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 let https = require('https')
-
+const dotenv = require('dotenv')
+const path = require('path')
+dotenv.config()
 https.createServer(() => {
 })
+
+console.log(process.pid);
+console.log(process.env.PORT);
+console.log(process.env.NODE_ENV);
+
+// ----------------------------
+const siteURL = 'http://localhost:8080/users?id=5252'
+const url = new URL(siteURL)
